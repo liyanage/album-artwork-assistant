0a6d9f819744dd21a6b53d7b49bd2172dcd52eac
liyanage <liyanage@153f8dbc-cef0-0310-8e0e-ba1d6c9f8c6b>
2008-11-19 19:35:16 +0000

    album-artwork-assistant: help content updates
    git-svn-id: svn+ssh://www.entropy.ch/Users/liyanage/Documents/svnroot/trunk/album-artwork-assistant@407 153f8dbc-cef0-0310-8e0e-ba1d6c9f8c6b

---
+++ Resources/Album Artwork Assistant Help/update-help.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+echo updating help content in $PWD
+
+WEBSITE=http://www.entropy.ch/software/macosx/album-artwork-assistant/
+OUTFILE="Album Artwork Assistant Help.html"
+
+curl $WEBSITE | xsltproc 2>/dev/null -o "$OUTFILE" --nonet --stringparam website $WEBSITE update-help.xslt -
+
+IMAGES=$(xsltproc --nonet --stringparam website $WEBSITE extract-images.xslt "$OUTFILE" 2>/dev/null)
+
+for i in $IMAGES; do
+
+curl -O $i
+
+done
