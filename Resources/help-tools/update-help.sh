a5795c3c348ecbe4823e6bc5c2bf610d943c1532
Marc Liyanage <reg.gitcommit@entropy.ch>
2008-11-27 08:47:56 +0000

    album-artwork-assistant: localized help build automation
    git-svn-id: svn+ssh://www.entropy.ch/Users/liyanage/Documents/svnroot/trunk/album-artwork-assistant@419 153f8dbc-cef0-0310-8e0e-ba1d6c9f8c6b

---
+++ Resources/help-tools/update-help.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+LANG=$1
+OUTDIR="$2"
+WD=$PWD
+
+WEBSITE=http://www.entropy.ch/software/macosx/album-artwork-assistant/
+OUTFILE="Album Artwork Assistant Help.html"
+
+curl -s $WEBSITE | xsltproc 2>/dev/null \
+	-o "$OUTDIR/$OUTFILE" \
+	--nonet \
+	--stringparam website $WEBSITE \
+	--stringparam lang $LANG \
+	"$WD/Resources/help-tools/update-help.xslt" -
+
+cd "$OUTDIR"
+
+IMAGES=$(xsltproc --nonet --stringparam website $WEBSITE "$WD/Resources/help-tools/extract-images.xslt" "$OUTFILE" 2>/dev/null)
+
+for i in $IMAGES; do
+	curl -s -O $i
+done
