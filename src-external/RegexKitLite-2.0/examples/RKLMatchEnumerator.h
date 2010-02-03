a2c9ff29b6bd1191646c9df1c45bffdb004a5b00
Marc Liyanage <liyanage@liyanage1.apple.com>
2010-02-03 09:17:54 -0800

    update regexkit-lite from 2.0 to 3.3

--- src-external/RegexKitLite-2.0/examples/RKLMatchEnumerator.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/NSEnumerator.h>
-#import <Foundation/NSString.h>
-#import <stddef.h>
-
-@interface NSString (RegexKitLiteEnumeratorAdditions)
-- (NSEnumerator *)matchEnumeratorWithRegex:(NSString *)regex;
-@end
