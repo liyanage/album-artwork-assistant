194319329319fab1b5d6056665cfb6fabae7ccc5
Marc Liyanage <liyanage@primavera.local>
2010-04-11 13:32:56 -0700

    Improved alert messages when iTunes AppleScript installation fails.

--- extract-aws-secret-key-from-keychain.pl
+++ extract-aws-secret-key-from-keychain.pl
@@ -15,10 +15,9 @@ my @values = map {ord($_)} split(//, $gzipped);
 
 my $builddir = $ENV{CONFIGURATION_BUILD_DIR} || '';
 
-die "CONFIGURATION_BUILD_DIR '$builddir' invalid" unless (-d $builddir);
-die "Unable to mkdir output dir" if (system(qq(mkdir -p "$ENV{CONFIGURATION_BUILD_DIR}/include")) >> 8);
+die "Unable to mkdir output dir" if (system(qq(mkdir -p "$builddir/include")) >> 8);
 
-my $file = IO::File->new(">$ENV{CONFIGURATION_BUILD_DIR}/include/amazon_aws_secret_key.h");
+my $file = IO::File->new(">$builddir/include/amazon_aws_secret_key.h");
 die unless $file;
 $file->print("#define AMAZON_AWS_SECRET_KEY_BYTES {" . join(', ', @values) . "}\n");
 $file->print("#define AMAZON_AWS_SECRET_KEY_LENGTH " . @values . "\n");
